937315cc1944146a08f19a61df43f7bdb3ddc94c
Brian Silverman <bri@raspberrystem.com>
2015-06-17 13:23:41 -0400

    projects - support no line numbers for code blocks

--- im/codeblocks.js
+++ im/codeblocks.js
@@ -2,14 +2,17 @@ window.onload = function() {
     var codeblocks = document.getElementsByClassName("code");
     for (i = 0; i < codeblocks.length; i++) { 
         codeblock = codeblocks[i].children[1];
-        firstline = codeblocks[i].getAttribute("data-firstline");
-        if (! firstline) {
-            firstline = 1;
+        firstLineNumber = codeblocks[i].getAttribute("data-firstline");
+        lineNumberFormatter = function (line) {return String(line)};
+        if (firstLineNumber == null) {
+            firstLineNumber = 1;
+        } else if (firstLineNumber == 0) {
+            lineNumberFormatter = function (line) {return ""};
         }
 
         // Split into lines and delete last line.  Then rejoin.
         // Rationale: its easier to keep the code blocks in the HTML when the
-        // top and bottom lines are stripped.
+        // bottom line is stripped.
         var lines = codeblock.value.split('\n');
         lines.splice(-1,1);
         numLines = lines.length;
@@ -21,8 +24,9 @@ window.onload = function() {
                 version: 3,
                 singleLineStringErrors: false
             },
-            firstLineNumber: Number(firstline),
+            firstLineNumber: Number(firstLineNumber),
             lineNumbers: true,
+            lineNumberFormatter: lineNumberFormatter,
             theme: "solarized-dark",
             readOnly: true,
             textWrapping: true,
